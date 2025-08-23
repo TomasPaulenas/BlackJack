94d453cfdb60f31fb6ec6b1591bb5d137f79e14d
Tomas <tomaspaulenas@hotmail.com>
2025-08-24 00:11:49 +0200

    feat: add new feactures

--- Assets/js/juego.js
+++ Assets/js/juego.js
@@ -7,7 +7,9 @@
 let deck         = [];
 const tipos      = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
+  
 
+//this function creates a new deck
 const crearDeck = () => {
 
     for( let i = 2; i <= 10; i++){
@@ -23,12 +25,37 @@ const crearDeck = () => {
             deck.push( especial + tipo)
         }   
     }
-    console.log(deck)
+    //console.log(deck)
 
     deck =_.shuffle(deck);
-
     console.log(deck)
+    return deck;
 
-       
 }
 crearDeck();
+
+
+//This function allows us to take a card from the deck
+const pedirCarta = () => {
+
+    if( deck.length === 0 ){
+        throw 'There is no more cards in the deck'
+    }
+
+    const carta = deck.pop()
+    
+    console.log(deck);
+    console.log(carta);  //The card has to be from the deck
+    return carta;
+} 
+
+//pedirCarta();
+const valorCarta = ( carta ) =>{
+ 
+    const valor = carta.substring(0, carta.length - 1 );
+    return (isNaN( valor )) ?
+    (valor === 'A') ? 11 : 10
+    : valor * 1;
+}
+const valor = valorCarta(pedirCarta());
+console.log({valor});
